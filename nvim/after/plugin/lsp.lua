4d0df2f09024413b39d9238ef55a5218e829060b
Alexey Kovalev <kovalev.al@phystech.edu>
2024-04-18 01:44:33 +0300

    fix nvim config

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -19,11 +19,26 @@ require("lspconfig").clangd.setup {
     capabilities = capabilities,
     cmd = {
         "/usr/bin/clangd",
-        "--enable-config",
+        -- "--enable-config",
         "--header-insertion=never",
         "--function-arg-placeholders=0"
-    }
+    },
+    -- commands = {
+    --     ClangdSwitchSourceHeader = {
+    --         function()
+    --             local bufnr = require'lspconfig'.util.validate_bufnr(0)
+    --             local params = { uri = vim.uri_from_bufnr(bufnr) }
+    --             vim.lsp.buf_request(bufnr, 'textDocument/switchSourceHeader', params, function(err, _, result)
+    --                 if err then error(tostring(err)) end
+    --                 if not result then print ("Corresponding file can’t be determined") return end
+    --                 vim.api.nvim_command("edit "..vim.uri_to_fname(result))
+    --                 vim.api.nvim_command("bdelete "..tostring(bufnr))
+    --             end)
+    --         end
+    --     },
+    -- }
 }
 
 lsp.setup()
 
+vim.keymap.set("n", "<leader>s", ":w<CR>:ClangdSwitchSourceHeader<CR>")
--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,17 +1,29 @@
 local builtin = require('telescope.builtin')
 local actions = require('telescope.actions')
 
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+-- vim.keymap.set('n', '<leader>fb', require 'telescope'.extensions.file_browser.file_browser, {})
 
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fd', function()
     builtin.find_files({ hidden = true })
 end)
-
 vim.keymap.set('n', '<leader>fr', builtin.oldfiles, {})
-
--- vim.keymap.set('n', '<leader>fb', require 'telescope'.extensions.file_browser.file_browser, {})
-
 vim.keymap.set('n', '<leader>fb', ':Telescope file_browser path=%:p:h select_buffer=true<CR>', {})
+vim.keymap.set('n', '<leader>fg', ':Telescope live_grep<CR>', {})
+vim.keymap.set('n', '<leader>gs', ':Telescope grep_string<CR>', {})
+vim.keymap.set('n', '<leader>lg', ':Telescope live_grep<CR>', {})
+
+vim.keymap.set('n', 'ff', builtin.find_files, {})
+vim.keymap.set('n', 'fd', function()
+    builtin.find_files({ hidden = true })
+end)
+vim.keymap.set('n', 'fr', builtin.oldfiles, {})
+vim.keymap.set('n', 'fb', ':Telescope file_browser path=%:p:h select_buffer=true<CR>', {})
+vim.keymap.set('n', 'fg', ':Telescope live_grep<CR>', {})
+vim.keymap.set('n', 'gs', ':Telescope grep_string<CR>', {})
+vim.keymap.set('n', 'gp', ':Telescope live_grep<CR>', {})
+
+vim.keymap.set('n', 'gt', builtin.lsp_type_definitions, {})
 
 require('telescope').setup({
     defaults = {
